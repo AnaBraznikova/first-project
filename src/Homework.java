1682b6bef7ac8cdaf8f92d5d38b6692b52fbfd0d
AnaBraznikova <a-brazanikova93@rambler.ru>
2022-11-08 20:59:23 +0100

    i send files

---
+++ src/Homework.java
@@ -0,0 +1,15 @@
+public class Homework {
+    public static void main(String[] args) {
+
+        System.out.println("Я умею выводить строчки");
+        System.out.println("Я начал программировать");
+
+
+
+
+        System.out.println(67 / 4);
+        System.out.println(450 * 2);
+        System.out.println(1234 * 1234);
+
+    }
+}
